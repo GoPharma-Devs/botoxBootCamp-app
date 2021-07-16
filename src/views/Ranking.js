9e2582d8e40844a27d451e360bca51e97cb2504b
Carlos Boyzo <carlosboyzo@iMac-de-Carlos.local>
2021-07-16 14:55:58 -0500

    fix

--- src/views/Ranking.js
+++ src/views/Ranking.js
@@ -1,4 +1,4 @@
-import React, { useState,  } from "react";
+import React from "react";
 import Navbar from "../components/Navbar";
 import Logo from "../assets/logo.png";
 import Cookies from "universal-cookie";

22dbe4ab6ba251102ca8ade78b29594144ad7cde
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2021-07-19 01:52:44 -0500

    fragment gracias

--- src/views/Gracias.js
+++ src/views/Gracias.js
@@ -3,14 +3,14 @@ import Navbar from '../components/Navbar'
 
 function Gracias() {
   return (
-   <>
+   <React.Fragment>
    <Navbar/>
     <section>
       <div className="contendor">
         <h1>¡GRACIAS!</h1>
       </div>
     </section>
-   </>
+   </React.Fragment>
   )
 }
 

f6bb05e294310a89ddda0c0340a62b0d53a44960
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos.local>
2021-07-19 02:24:29 -0500

    ajuste reset

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,18 +1,19 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import Logo from '../assets/logo.png'
 import Butulinica from '../assets/butulinica.png'
 import Allergan from '../assets/allergan.png'
 import Boton from '../assets/boton.png'
 function Home() {
+  const handleClick = () => {
+    window.open("https://campus.botoxbootcamp.com.mx/acceso.php","_self");
+  };
   return (
     <section className="home">
       <div className="contenedor-logo">
         <img className="logo" src={Logo} alt="Botox-BootCamp 2021" />
       </div>
-      <Link
-      className="btn-flotante"
-        to={{ pathname: "/login"}}> <img src={Boton} alt="" /></Link>
+      <button  className="btn-flotante" onClick={handleClick} ><img src={Boton} target="_self" alt="" /></button>
       <div className="contenedor-patrocinadores">
         <img src={Butulinica} alt="Botox-BootCamp 2021" />
         <img  src={Allergan} alt="Botox-BootCamp 2021" />
